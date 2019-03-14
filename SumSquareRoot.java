c4dcd16f1d9cd065ef16dc149c4168e207845d34
dcoleman7 <48440212+dcoleman7@users.noreply.github.com>
2019-03-14 19:55:26 -0400

    Update SumSquareRoot.java

--- SumSquareRoot.java
+++ SumSquareRoot.java
@@ -1,1 +1,19 @@
+package Chapter5;
 
+public class SumSquare {
+	public static void main(String[] args) {
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		
+		int num = 3;
+		int cummulation = 9;
+		while (num <= 33) {
+			System.out.printf("\nCummulation result: %-6d : %4d %4s %4d", cummulation, num, '*', num);
+			num = num + 3;
+			if(num <= 33) {
+				cummulation = num * num + cummulation;
+			}
+			else break;
+		}
+		System.out.println("\nThes SUM of square of multiples of 3 is " + cummulation);
+	}
+}
